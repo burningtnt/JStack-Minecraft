88b2ed98301443b42ddcd9da6f30e34749f8865a
Burning_TNT <88144530+burningtnt@users.noreply.github.com>
2023-02-26 19:31:15 +0800

    Use JRE

--- release/Jstack Minecraft/Run_Windows.bat
+++ release/Jstack Minecraft/Run_Windows.bat
@@ -1,3 +1,3 @@
-java --add-exports jdk.internal.jvmstat/sun.jvmstat.monitor=ALL-UNNAMED --add-exports jdk.attach/sun.tools.attach=ALL-UNNAMED -jar "Jstack Minecraft.jar"
+jre\bin\java.exe --add-exports jdk.internal.jvmstat/sun.jvmstat.monitor=ALL-UNNAMED --add-exports jdk.attach/sun.tools.attach=ALL-UNNAMED -jar "Jstack Minecraft.jar"
 
 pause
